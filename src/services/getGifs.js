86c2ab7c78b1af9dc7fa1030e0a397c459fe1b05
Erick <eeramirez@tuvansa.com.mx>
2023-12-31 19:00:05 -0600

    archivo .env

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,10 +1,11 @@
+import { getEnvVariables } from "../helpers/getEnvVariables"
 
-const apiKey = 'q9a2LzQcB9b1CgVR6C4tsl20HQ5MU2TJ'
+const { VITE_GIPHY_APY_KEY } = getEnvVariables()
 
 
 export const getGifs = async ({ keyword = 'the boys'} = {}) => {
 
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${VITE_GIPHY_APY_KEY}&q=${keyword}&limit=10&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
 
   return fetch(apiURL)
     .then(resp => resp.json())
